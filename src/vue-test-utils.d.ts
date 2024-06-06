fd5c11605e0f8bb06873d6b7df8b4718a0cd4bed
Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>
2024-06-06 10:10:55 -0400

    feat(eslint): eslint v9 and shared configs (#59)

--- src/vue-test-utils.d.ts
+++ src/vue-test-utils.d.ts
@@ -2,6 +2,6 @@ import type { DOMWrapper } from '@vue/test-utils'
 
 declare module '@vue/test-utils' {
   export class VueWrapper {
-    findTestId<K extends keyof HTMLElementTagNameMap>(dataTestid: string): DOMWrapper<HTMLElementTagNameMap[K]>;
+    findTestId<K extends keyof HTMLElementTagNameMap>(dataTestid: string): DOMWrapper<HTMLElementTagNameMap[K]>
   }
 }
