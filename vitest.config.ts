333bc54dd12edd093de800037b33a677c1c2e9ea
Adam DeHaven <adam.dehaven@konghq.com>
2024-12-02 11:07:33 -0500

    feat(*): initial commit :tada:

---
+++ vitest.config.ts
@@ -0,0 +1,28 @@
+import { fileURLToPath } from 'node:url'
+import { mergeConfig, defineConfig } from 'vitest/config'
+import viteConfig from './vite.config'
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      include: ['**/*.spec.ts'],
+      exclude: [
+        './dist/**',
+        'node_modules',
+      ],
+      setupFiles: ['./vitest.setup.ts'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      deps: {
+        optimizer: {
+          web: {
+            // https://github.com/vitest-dev/vitest/issues/4074
+            exclude: ['vue'],
+          },
+        },
+      },
+    },
+  }),
+)
